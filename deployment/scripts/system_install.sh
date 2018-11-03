7a7be74a68a8d8dc920a92db7f89d2485a940741
Daniel Martinez <dmartinezmont@gmail.com>
2018-11-03 12:11:39 -0500

    Create more accessible logs

--- deployment/scripts/system_install.sh
+++ deployment/scripts/system_install.sh
@@ -14,5 +14,15 @@ pip install --upgrade awscli
 # Install Tomcat
 yum install -y tomcat
 
+# Add welcome message
+if ! grep -q "Banner /etc/ssh/sshd_banner" /etc/ssh/sshd_config; then
+	echo 'Banner /etc/ssh/sshd_banner' >> /etc/ssh/sshd_config
+fi
+echo "$(figlet $DEPLOYMENT_GROUP_NAME)" > /etc/ssh/sshd_banner
+systemctl reload sshd
+
+# Codedeploy logs
+ln -sfn /opt/codedeploy-agent/deployment-root/deployment-logs/codedeploy-agent-deployments.log /home/centos/codedeploy-agent-deployments.log
+
 # Clean up installation folder (multiple environments, same sever)
 rm -rf /opt/api-codedeploy
