e0de13e9db3cea59053949945d09da55e43c8d4d
Daniel Martínez <dmartinezmont@gmail.com>
2019-01-28 17:12:27 -0500

    Update tomcat_configure.sh

--- deployment/scripts/tomcat_configure.sh
+++ deployment/scripts/tomcat_configure.sh
@@ -6,5 +6,3 @@ mkdir -p $WORKSPACE/healthcheck
 cp $CODEDEPLOY/deployment/configs/healthcheck.html $WORKSPACE/healthcheck/index.html
 
 cp -n $CODEDEPLOY/deployment/configs/*.xml /etc/tomcat/Catalina/localhost/
-
-cp $CODEDEPLOY/deployment/configs/setenv.sh $TOMCAT_HOME/bin/setenv.sh
