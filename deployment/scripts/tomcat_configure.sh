b8b66257f415e67e6b1f3b150ba56f2eb290932c
Daniel Martínez <dmartinezmont@gmail.com>
2019-10-22 16:38:25 -0500

    Update tomcat_configure.sh

--- deployment/scripts/tomcat_configure.sh
+++ deployment/scripts/tomcat_configure.sh
@@ -6,5 +6,6 @@ mkdir -p $WORKSPACE/healthcheck
 cp $CODEDEPLOY/deployment/configs/healthcheck.html $WORKSPACE/healthcheck/index.html
 
 if [ -f $CODEDEPLOY/deployment/configs/context.xml ]; then
+  echo 'Copying context.xml file'
   cp $CODEDEPLOY/deployment/configs/context.xml $TOMCAT_HOME/conf/Catalina/localhost/$DEPLOY_ENV.xml
 fi
